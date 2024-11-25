1ae624ac908caef443d5ff9c0ac299effb981c75
Cris Silva Jr. <crisjr@pm.me>
2024-11-25 11:17:06 +0000

    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2024
+
+- [Official link](https://adventofcode.com)
---
+++ template/main.rs
@@ -0,0 +1,12 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+    let result;
+
+    for line in stdin.lock().lines() {
+    }
+
+    println!("{}", result);
+}
+
