e9f76bbdcf72d5fcb95a23594b9f30f94ba5e9d5
Cris Jr <crisjr@pm.me>
2024-12-01 11:43:18 +0000

    day 1, part 2

--- 01/main.rs
+++ 01/main.rs
@@ -1,23 +1,24 @@
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 
-fn compute_difference(list_a: &Vec<i32>, list_b: &Vec<i32>, length: usize) -> i32 {
-    let mut result: i32 = 0;
+fn compute_similarity(list_a: &Vec<i32>, list_b_count: &HashMap<i32, i32>) -> i32 {
+    let mut similarity: i32 = 0;
 
-    for i in 0..length {
-	result += (list_a[i] - list_b[i]).abs();
+    for a in list_a {
+	similarity += a * list_b_count.get(&a).copied().unwrap_or(0);
     }
 
-    return result;
+    return similarity;
 }
 
 fn main() {
     let stdin = io::stdin();
     let mut line: String;
     let mut list_a: Vec<i32> = vec![];
-    let mut list_b: Vec<i32> = vec![];
+    let mut list_b_count: HashMap<i32, i32> = HashMap::<i32, i32>::new();
     let mut which_list: i32 = 0;
     let mut list_part: i32;
-    let mut length: usize = 0;
+    let mut old_value: i32;
     let result: i32;
 
     for raw_line in stdin.lock().lines() {
@@ -29,18 +30,15 @@ fn main() {
 		if which_list % 2 == 0 {
 		    list_a.push(list_part);
 		} else {
-		    list_b.push(list_part);
+		    old_value = list_b_count.get(&list_part).copied().unwrap_or(0);
+		    list_b_count.insert(list_part, 1 + old_value);
 		}
 		which_list += 1;
 	    }
-	    length += 1;
 	}
     }
 
-    list_a.sort();
-    list_b.sort();
-
-    result = compute_difference(&list_a, &list_b, length);
+    result = compute_similarity(&list_a, &list_b_count);
     println!("{}", result);
 }
 
