253169485868f5192028235f06d9832a7c82e628
Cris Jr <crisjr@pm.me>
2024-12-02 09:29:56 +0000

    day 2, part 1

---
+++ 02/main.rs
@@ -0,0 +1,62 @@
+use std::io::{self, BufRead};
+
+fn parse_input(line: String) -> Vec<i32> {
+    let inlet = line.split_whitespace();
+    let mut outlet: Vec<i32> = vec![];
+    let mut out_s: i32;
+
+    for in_s in inlet {
+        out_s = in_s.parse::<i32>().unwrap();
+        outlet.push(out_s);
+    }
+
+    return outlet;
+}
+
+fn is_report_safe(report: &Vec<i32>) -> bool {
+    let mut first_comparison: bool = true;
+    let mut is_increasing_overall: bool = true;
+    let mut is_increasing: bool;
+    let mut difference: i32;
+    let limit = report.len();
+
+    for i in 1..limit {
+        difference = report[i] - report[i - 1];
+        is_increasing = difference > 0;
+
+        if first_comparison {
+            first_comparison = false;
+            is_increasing_overall = is_increasing;
+        }
+
+        if is_increasing_overall != is_increasing {
+            return false;
+        }
+
+        if difference.abs() < 1 || difference.abs() > 3 {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut line: String;
+    let mut result: i32 = 0;
+    let mut report: Vec<i32>;
+
+    for raw_line in stdin.lock().lines() {
+        line = raw_line.unwrap();
+        if line.len() > 0 {
+            report = parse_input(line);
+            if is_report_safe(&report) {
+                result += 1;
+            }
+        }
+    }
+
+    println!("{}", result);
+}
+
